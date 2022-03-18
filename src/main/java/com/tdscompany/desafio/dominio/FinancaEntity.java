df0e5dcd39e7c923962adbbb7fc95b99e50fbd6a
RobertAraujo <endriosrobert@gmail.com>
2022-03-17 19:29:56 -0500

    Criação da entidade Financeira

--- src/main/java/com/tdscompany/desafio/dominio/FinancaEntity.java
+++ src/main/java/com/tdscompany/desafio/dominio/FinancaEntity.java
@@ -1,4 +1,12 @@
 package com.tdscompany.desafio.dominio;
 
+import java.time.LocalDateTime;
+
 public class FinancaEntity {
+
+    private String nome;
+    private LocalDateTime dataEntrada;
+    private LocalDateTime dataVencimento;
+    private TagEntity tipoTag;
+    private PessoaEntity pessoa;
 }
